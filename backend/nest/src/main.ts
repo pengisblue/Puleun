88286fc6a7a79627c6f98a11c20f43d0a2f11f28
JongKook <whdrnrdl789@naver.com>
2024-01-24 11:30:32 +0900

    feat: CORS
    Change-Id: Iad1302c690d5161fdbb16a6ec42f9a3c3380e9e9

--- backend/nest/src/main.ts
+++ backend/nest/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: "192.168.30.*",
+    credentials: true,
+    exposedHeaders: ['Authorization'], // * 사용할 헤더 추가.
+  });
   await app.listen(3000);
 }
 bootstrap();
