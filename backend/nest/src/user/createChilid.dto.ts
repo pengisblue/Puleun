ba4267662a4c03816117217d231ddc50c3b55939
최진우 <chjnw0204@gmail.com>
2024-01-24 16:57:12 +0900

    feat:user api description
    Change-Id: I85055904db1c17dc950fde3fcf41fba15f53c62a

--- backend/nest/src/user/createChilid.dto.ts
+++ backend/nest/src/user/createChilid.dto.ts
@@ -1,15 +1,21 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsOptional, IsString } from "class-validator";
 import { User } from "src/user/user.entity";
 
 export class CreateChildDto{
+    @ApiProperty()
     nickname: string;
 
+    @ApiProperty()
     birth_DT: Date;
 
+    @ApiProperty()
     gender: string;
 
+    @ApiProperty()
     parent: User;
 
+    @ApiProperty()
     @IsOptional()
     @IsString()
     profile_img_url?: string="";
