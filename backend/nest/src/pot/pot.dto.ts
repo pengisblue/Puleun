8883cf6a4458e505aff5a32eccc20576c431fc5e
박종국 <whdrnrdl789@naver.com>
2024-01-25 14:04:36 +0900

    feat: Pot CRUD
    Change-Id: I0bfffcd939d24ab2a2cdcbbbae492d95a06611ab

--- backend/nest/src/pot/pot.dto.ts
+++ backend/nest/src/pot/pot.dto.ts
@@ -42,12 +42,55 @@ export class CreatePotDto {
     @IsInt()
     @IsOptional()
     happy_cnt ? : number;
-
-    
-
-
 }
 
 export class UpdatePotDto{
+    
+}
 
+export class SelectPotDto{
+    @IsString()
+    potName: string;
+
+    @IsString()
+    potSpecies: string;
+
+    @IsDate()
+    @IsOptional()
+    createdAt ? : Date
+
+    @IsDate()
+    @IsOptional()
+    updatedAt ? : Date
+    
+    @IsInt()
+    @IsOptional()
+    min_temperature?: number;
+
+    @IsInt()
+    @IsOptional()
+    max_temperature: number;
+
+    @IsInt()
+    @IsOptional()
+    min_moisture: number;
+
+    @IsInt()
+    @IsOptional()
+    max_moisture: number;
+
+    @IsString()
+    @IsOptional()
+    @Length(1, 200)
+    pot_img_url: string;
+
+    @IsInt()
+    @IsOptional()
+    happy_cnt ? : number;
+
+    // user_id가 0 이라면 이 화분은 부모가 키우고 있다는 것!
+    // user_id가 존재한다면 아이가 키우고 있다는 것
+    @IsInt()
+    @IsOptional()
+    user_id ? : number = 0;
 }
