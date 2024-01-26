da35b6bba7a19ac9e79466c42ba50e77e8c425a9
최진우 <chjnw0204@gmail.com>
2024-01-26 14:26:31 +0900

    fix: modify typeorm table
    Change-Id: I355f58e680a15b4c68927177e2934f76db94c485

--- backend/nest/src/socket/socket.gateway.ts
+++ backend/nest/src/socket/socket.gateway.ts
@@ -1,8 +1,5 @@
-import { InjectRepository } from '@nestjs/typeorm';
 import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
-import { Sentence } from 'src/sentence/sentence.entity';
-import { Repository } from 'typeorm';
 
 @WebSocketGateway(8080, {
   cors: { origin: '*' },
@@ -11,11 +8,6 @@ import { Repository } from 'typeorm';
 export class SocketGateway {
   @WebSocketServer()
   server: Server;
-
-  constructor(
-    @InjectRepository(Sentence)
-    private readonly SentenceRepository: Repository<Sentence>
-  ){}
 
   @SubscribeMessage('login')
   handleClientConnect(@ConnectedSocket() client: Socket, @MessageBody('message') message: JSON) {
