bd89b8f5925e19676d93bab57794d8050aa0968f
JongKook <whdrnrdl789@naver.com>
2024-01-29 00:34:37 +0900

    feat: file Upload
    Change-Id: I4b0fc2c0562ade4d305d176601df9f6ddb2a2ea4

--- backend/nest/src/app.module.ts
+++ backend/nest/src/app.module.ts
@@ -13,6 +13,8 @@ import { SentenceModule } from './sentence/sentence.module';
 import { AlarmModule } from './alarm/alarm.module';
 import { CalenderCodeModule } from './calender-code/calender-code.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { FileGateway } from './file/file.gateway';
+import { FileController } from './file/file.controller';
 
 @Module({
   imports: [UserModule, PotModule, 
@@ -23,13 +25,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: '1234',
-      database: 'test',
+      password: 'root',
+      database: 'entity_test',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true, // 개발 환경에서만 사용 (production에서는 비활성화 권장)
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, FileController],
+  providers: [AppService, FileGateway],
 })
 export class AppModule {}
