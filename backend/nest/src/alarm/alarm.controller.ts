06febba074dafcabe8f5dd70171cc6268f5de1bc
JongKook <whdrnrdl789@naver.com>
2024-01-30 01:52:31 +0900

    feat: alarm API
    Change-Id: Ie0d39db13eb1f7255477df42ff9a343c6d33ba03

--- backend/nest/src/alarm/alarm.controller.ts
+++ backend/nest/src/alarm/alarm.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { AlarmService } from './alarm.service';
+import { Alarm } from './alarm.entity';
+import { ApiBody, ApiOperation, ApiProperty, ApiTags } from '@nestjs/swagger';
+import { AlarmDto } from './alarm.dto';
 
 @Controller('alarm')
-export class AlarmController {}
+@ApiTags('alarm')
+export class AlarmController {
+    constructor(private readonly alarmService:AlarmService){}
+
+    @Post()
+    @ApiOperation({ summary: "알람 저장"})
+    async save(@Body() alarm:Alarm, @Body() pot_id: number): Promise<number>{
+        await this.alarmService.addAlarm(alarm, pot_id);
+        return 1;
+    }
+
+    @Get(':pot_id')
+    @ApiOperation({summary: '식물의 모든 알람 조회'})
+    async findAll(@Param() pot_id: number): Promise<AlarmDto[]>{
+        return await this.alarmService.findByPotId(pot_id);
+    }
+
+    @Post(':alarm_id')
+    @ApiOperation({summary: '특정 알람 조회'})
+    @ApiBody({type:AlarmDto})
+    async alarmDetail(@Param() alarm_id: number): Promise<Alarm>{
+        return await this.alarmService.alarmDetail(alarm_id);
+    }
+
+    @Put(':alarm_id')
+    @ApiOperation({summary: '알람 수정'})
+    @ApiBody({type:AlarmDto})
+    async update(@Param() alarm_id:number, alarmDto: AlarmDto): Promise<number>{
+        await this.alarmService.updateAlarm(alarmDto, alarm_id);
+        return 1;
+    }
+
+    @Delete(':alarm_id')
+    @ApiOperation({summary: '알람 삭제'})
+    @ApiBody({type:AlarmDto})
+    async delete(@Param() alarm_id: number){
+        await this.alarmService.deleteAlarm(alarm_id);
+        return 1;
+    }
+
+
+}
--- backend/nest/src/alarm/alarm.service.ts
+++ backend/nest/src/alarm/alarm.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Alarm } from './alarm.entity';
+import { Repository } from 'typeorm';
+import { AlarmDto } from './alarm.dto';
 
 @Injectable()
-export class AlarmService {}
+export class AlarmService {
+    constructor(@InjectRepository(Alarm)private readonly alarmRepository: Repository<Alarm>){}
+
+    async addAlarm(alarm: Alarm, pot_id: number): Promise<AlarmDto>{
+        // const alarmDto = await this.potRepository.save(alarm);
+        return await this.alarmRepository.save(alarm);
+    }
+
+    async deleteAlarm(alarm_id: number){
+        await this.alarmRepository.delete(alarm_id);
+    }
+
+    async updateAlarm(alarm: AlarmDto, pot_id: number){
+        await this.alarmRepository.update(pot_id, {...alarm});
+    }
+
+    async findByPotId(pot_id: number): Promise<Alarm[]>{
+        return await this.alarmRepository.find({
+            where: {pot_id}
+        });
+    }
+
+    async alarmDetail(alarm_id: number): Promise<Alarm>{
+        return await this.alarmRepository.findOne({
+            where: {alarm_id}
+        });
+    }
+
+}
