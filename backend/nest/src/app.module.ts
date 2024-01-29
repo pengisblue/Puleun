8f753e80c63fe33275b7581fac9466c3888e485b
최진우 <chjnw0204@gmail.com>
2024-01-29 10:37:26 +0900

    fix: merge conflict
    Change-Id: I7b4c7d258b505a6389843512b14f218f6d788334

--- backend/nest/src/app.module.ts
+++ backend/nest/src/app.module.ts
@@ -13,12 +13,9 @@ import { SentenceModule } from './sentence/sentence.module';
 import { AlarmModule } from './alarm/alarm.module';
 import { CalenderCodeModule } from './calender-code/calender-code.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-<<<<<<< HEAD
 import { SocketGateway } from './socket/socket.gateway';
-=======
 import { FileGateway } from './file/file.gateway';
 import { FileController } from './file/file.controller';
->>>>>>> f3089ae9f133c66a65a1a0bfb2b4348641c95f3e
 
 @Module({
   imports: [UserModule, PotModule, 
@@ -29,18 +26,13 @@ import { FileController } from './file/file.controller';
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: 'root',
-      database: 'entity_test',
+      password: '1234',
+      database: 'test',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true, // 개발 환경에서만 사용 (production에서는 비활성화 권장)
     }),
   ],
-<<<<<<< HEAD
-  controllers: [AppController],
-  providers: [AppService, SocketGateway],
-=======
   controllers: [AppController, FileController],
-  providers: [AppService, FileGateway],
->>>>>>> f3089ae9f133c66a65a1a0bfb2b4348641c95f3e
+  providers: [AppService, SocketGateway, FileGateway],
 })
 export class AppModule {}
