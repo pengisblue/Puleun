5bc5805b38e60e9165df1ee27886989f3618ae9c
박종국 <whdrnrdl789@naver.com>
2024-01-29 15:23:03 +0900

    feat: swagger and file upload download
    Change-Id: I519aaad08d94f595474d936a5b422081ee02ba62

--- backend/nest/src/app.module.ts
+++ backend/nest/src/app.module.ts
@@ -13,11 +13,10 @@ import { SentenceModule } from './sentence/sentence.module';
 import { AlarmModule } from './alarm/alarm.module';
 import { CalenderCodeModule } from './calender-code/calender-code.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { FileGateway } from './file/file.gateway';
-import { FileController } from './file/file.controller';
+import { FileModule } from './file/file.module';
 
 @Module({
-  imports: [UserModule, PotModule, 
+  imports: [UserModule, PotModule, FileModule,
     PotStateModule, SpeciesModule, UserLoginModule, DeviceModule, 
     CalenderModule, TalkModule, SentenceModule, AlarmModule, CalenderCodeModule,
     TypeOrmModule.forRoot({
@@ -25,13 +24,13 @@ import { FileController } from './file/file.controller';
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: 'root',
-      database: 'entity_test',
+      password: '1234',
+      database: 'test',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true, // 개발 환경에서만 사용 (production에서는 비활성화 권장)
     }),
   ],
-  controllers: [AppController, FileController],
-  providers: [AppService, FileGateway],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
