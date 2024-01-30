619bc1a1b1fbcfe5ab1bf335d0f57f0e0ebead42
김연빈 <kyb1434@gmail.com>
2024-01-30 18:02:52 +0900

    feat: 음성파일 받기 socket 통신 하면서 같이 하도록 함수 생성
    Change-Id: Ideea690ad5a23514ed93674eaa338bfa6fcf2d28

--- embedded/RaspberryPi/src/socket_client.py
+++ embedded/RaspberryPi/src/socket_client.py
@@ -1,6 +1,7 @@
 import socketio
 from dotenv import load_dotenv
 import os
+import requests
 
 
 load_dotenv()
@@ -18,6 +19,14 @@ def connect():
 @sio.event
 def disconnect():
     print('Disconnect from server')
+
+# 얘도 이벤트 핸들러가 필요한가? ㄴㄴ
+def tts(): # 음성파일 받기
+    mp3 = requests.get("http://192.168.30.209:3000/file/ETA.mp3")
+    goToServer = requests.post("http://192.168.209.194:3000/file")
+    open("sample.mp3", "wb").write(mp3.content)
+
+
 
 if __name__ == '__main__': # 메인 실행문
     server_url = os.getenv('SERVER_URL')
