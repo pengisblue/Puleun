e0e6d2e0feeee2993989e4c27a2db7cc97e12e3b
최진우 <chjnw0204@gmail.com>
2024-01-31 16:41:43 +0900

    socket success
    Change-Id: Icc23fa30dcfa2722647b20aef9707ec42ed64981

--- backend/nest/src/socket/socket.service.ts
+++ backend/nest/src/socket/socket.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from "@nestjs/typeorm";
+import { InjectRepository } from '@nestjs/typeorm';
 import { CreatePotStateDto } from "src/pot-state/pot-state-insert.dto";
 import { PotState } from "src/pot-state/pot-state.entity";
 import { Repository } from "typeorm";
+import * as fs from 'fs';
+import { error } from 'console';
+import { ExceptionsHandler } from '@nestjs/core/exceptions/exceptions-handler';
 
 @Injectable()
 export class SocketService {
@@ -17,19 +20,25 @@ export class SocketService {
         return 1;
     }
 
-    // 대화 저장
-    async saveSentence(){
+    async stt(txt: string, base64Data: string): Promise<string>{
+        // txt 파일 저장 -> redis
 
+        // 답변 생성 (gpt api) -> sentence // 여기가 시간제일 많이
+        // message -> tts >> wav // 시간 안해봐서 모름
+        
+        // client.emit(wav) 
+        const filePath = "./upload/2024-01-30/" + "ETA.mp3"
+        const content = await new Promise<Buffer>((resolve, reject) => {
+            fs.readFile(filePath, (err, data) => {
+              if (err) {
+                reject(err)
+              } else {
+                resolve(data)
+              }
+            })
+        })
+        if (typeof content == typeof error) throw ExceptionsHandler
+        
+        return Buffer.from(content).toString('base64')
     }
-    
-    //파일 저장
-    async saveFile(){
-
-    }
-
-    //파일 전송
-    async uploadFile(){
-
-    }
-
 }
