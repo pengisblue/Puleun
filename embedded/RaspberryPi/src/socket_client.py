825ee27730636b59578b4ee8a05b578334d3fa90
김연빈 <kyb1434@gmail.com>
2024-01-31 16:19:37 +0900

    feat: socket receive
    tts: wav파일 디코딩 후 재생
    owner_change: 주인 유무 변경
    refresh: 새로고침 신호 받기
    
    Change-Id: Ifc0ce306169865f14a9e5db9cb21930fba58bfee

--- embedded/RaspberryPi/src/socket_client.py
+++ embedded/RaspberryPi/src/socket_client.py
@@ -2,13 +2,16 @@ import socketio
 from dotenv import load_dotenv
 import os
 import requests
+import base64
+import time
+import pygame
 
 
 load_dotenv()
 
 sio = socketio.Client()
 
-pot_id = None # 디바이스 id
+pot_id = None # 식물 id
 is_owner = False # 주인 연결 여부
 is_connected = False # 백과 연결 여부
 
@@ -33,12 +36,46 @@ def disconnect():
 #---------------- 받기 --------------
 
 @sio.on('tts')
-def tts(): # 음성 파일 받기
-    pass
+def tts(data): # 음성 파일 받기
+    # .wav 디코딩해서 재생하기
+    
+    print("receive")
+    base64_data = data['base64Data']
+
+  # base64 디코딩
+    file_data = base64.b64decode(base64_data)
+
+    # 파일로 저장 (ex: received_file.wav)
+    with open('received_file.wav', 'wb') as file:
+        file.write(file_data)
+
+    print("File received and saved.")
+
+    # 오디오 재생
+    pygame.mixer.init()
+    file_path = "/received_file.wav"
+
+    try:
+        pygame.mixer.music.load(file_path)
+        pygame.mixer.music.play()
+        
+        # 파일 재생이 완료될 때까지 대기
+        while pygame.mixer.music.get_busy():
+            pygame.time.Clock().tick(10)
+
+    except pygame.error as e:
+        print("오류 발생:", e)
+    finally:
+        print("File play done")
+        pygame.mixer.quit()
 
 
-@sio.on('')
+@sio.on('refresh')
 def refresh(): # 새로고침 신호
+    # state로 지금 측정한 데이터를 보내주면 됨
+    # 아두이노야 측정해줘
+    # 측정값을 받아서 보낸다. > pot_state
+    print("refreshing...")
     pass
 
 
@@ -46,6 +83,11 @@ def refresh(): # 새로고침 신호
 def emotion(): # 표정 상태값
     pass
 
+
+@sio.on('owner_change')
+def owner_change(data): # 주인 변했을때 == 주인이 생겼을때/없어졌을때
+    is_owner = data
+    print("owner status changed")
 
 #--------------- 보내기 ---------------
 
