bd3dee70c9267bc12c5126734f230660ee02abc3
김해인 <khi762@naver.com>
2024-02-01 16:19:16 +0900

    feat: 애칭 입력 state 설정
    Change-Id: Ifd988e676fddbb77184dd4e3a30697083eed9851

--- frontend/src/pages/CreatePot.jsx
+++ frontend/src/pages/CreatePot.jsx
@@ -5,9 +5,16 @@ import { useState } from "react";
 
 export default function CreatePot() {
   const [selectedUser, setSelectedUser] = useState(null);
+  const [potName, setPotName] = useState(null);
 
+  // 주인 선택
   const handleUserChange = (value) => {
     setSelectedUser(value);
+  };
+
+  // 화분 애칭
+  const handlePotNameInput = (event) => {
+    setPotName(event.target.value)
   };
 
   return (
@@ -33,6 +40,8 @@ export default function CreatePot() {
             allTarget={false}
           />
         </section>
+
+        {/* 임시 확인용 */}
         <span>{selectedUser}</span>
 
         {/* 애칭 입력 */}
@@ -40,10 +49,14 @@ export default function CreatePot() {
           <label htmlFor="">식물 애칭</label>
           <input
             type="text"
+            onInput={handlePotNameInput}
             className="mt-2 block w-full rounded-md border-gray-300 text-gray-600 shadow-sm
             focus:border-amber-300 focus:ring focus:ring-amber-200 focus:ring-opacity-50"
           />
         </section>
+
+        {/* 임시 확인용 */}
+        <span>{potName}</span>
 
         {/* 품종 선택 */}
         <section>
