52dad3fa9921f4b8b127b91e87236dcd9a31143c
김해인 <khi762@naver.com>
2024-02-01 11:19:28 +0900

    feat: 메인화면에서 화분 목록으로 이동 버튼
    Change-Id: Ia9158baa69388bc5749672c522aed7c59a5369a0

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,14 +1,23 @@
 import PotDetailCard from "../components/Pots/PotDetailCard";
 import PotAddDetailCard from "../components/Pots/PotAddDetailCard";
 import { potDetailList } from "../test/potList";
+import chevron from "../asset/chevron-right.svg";
+import { useNavigate } from "react-router-dom";
 
 export default function MainPage() {
+  const navigate = useNavigate();
+
+  const goPotList = function() {
+    navigate("/pots")
+  }
+
   return (
     <>
       <div>
         <div>
-          <div className="m-2">
+          <div className="m-2 flex justify-between">
             <h1 className="text-2xl font-semibold">우리 화분</h1>
+            <img src={chevron} alt="goPotList" onClick={goPotList} />
           </div>
 
           {potDetailList.map((pot) => (
