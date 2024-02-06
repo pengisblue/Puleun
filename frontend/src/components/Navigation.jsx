07b13681a1be975e62e72b45ff898d3fc5934061
dongho <ghehd1125@gmail.com>
2024-02-06 15:42:55 +0900

    fix : Update navbar
    Change-Id: I6076862dd784fe39f06e67994e9d8b5fc885f10c

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -95,7 +95,7 @@ export default function Navigation() {
                       <Menu.Item>
                         {({ active }) => (
                           <a
-                            href="#"
+                            href="#!"
                             className={classNames(
                               active ? "bg-gray-100" : "",
                               "block px-4 py-2 text-sm text-gray-700",
@@ -108,7 +108,7 @@ export default function Navigation() {
                       <Menu.Item>
                         {({ active }) => (
                           <a
-                            href="#"
+                            href="#!"
                             className={classNames(
                               active ? "bg-gray-100" : "",
                               "block px-4 py-2 text-sm text-gray-700",
@@ -121,7 +121,7 @@ export default function Navigation() {
                       <Menu.Item>
                         {({ active }) => (
                           <a
-                            href="#"
+                            href="#!"
                             className={classNames(
                               active ? "bg-gray-100" : "",
                               "block px-4 py-2 text-sm text-gray-700",
