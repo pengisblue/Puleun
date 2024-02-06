96be6c4767abe83a3cb8e87faa0ab611751ec708
최진우 <chjnw0204@gmail.com>
2024-02-06 15:10:54 +0900

    docs: add swagger of SpeciesList
    Change-Id: Ie18c580908aae0316a817f3042695d1e8808af91

--- backend/nest/src/species/species.service.ts
+++ backend/nest/src/species/species.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Species } from './species.entity';
 import { Repository } from 'typeorm';
+import { SpeciesListDto } from './species-res.dto';
 
 @Injectable()
 export class SpeciesService {
     constructor(@InjectRepository(Species) private readonly speciesRepository: Repository<Species>){}
     
-    async speciesFind(): Promise<Species[]>{
+    async speciesFind(): Promise<SpeciesListDto[]>{
         return await this.speciesRepository.find();
     }
 }
