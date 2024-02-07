95f4b690c129215a02e19d5752e116cde096f1ed
JongKook <whdrnrdl789@naver.com>
2024-02-08 01:41:37 +0900

    feat: Cron Test
    Change-Id: I7152260411b6f191ade6996acb0271ce554aa13b

--- backend/nest/src/alarm/alarm.service.ts
+++ backend/nest/src/alarm/alarm.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Alarm } from './alarm.entity';
 import { Repository } from 'typeorm';
 import { AlarmDto, CreateAlarmDto } from './alarm.dto';
+import { Cron } from '@nestjs/schedule';
 
 @Injectable()
 export class AlarmService {
@@ -25,5 +26,11 @@ export class AlarmService {
             where: {alarm_id}
         });
     }
-
+    // 초 분 시 일 월 요일
+    // 0 - 6 일요일부터 토요일까지
+    // 0 30 7 * * 1,3,6 -> 월 수 토 7시 30분 00초에 실행
+    @Cron('0 * * * * *')
+    handleCron(){
+        console.log('Cron Test!');
+    }
 }
