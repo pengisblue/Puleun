a50f136ad2c5fd127f142c5f6e05cf279160d727
박종국 <whdrnrdl789@naver.com>
2024-02-07 10:10:51 +0900

    fix: redis import
    Change-Id: I41f6dafa78e80a5276cd14ff4a77508dc320d094

--- backend/nest/src/talk/talk.module.ts
+++ backend/nest/src/talk/talk.module.ts
@@ -4,9 +4,10 @@ import { TalkService } from './talk.service';
 import { UserModule } from 'src/user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Talk } from './talk.entity';
+import { RedisModule } from 'src/redis/redis.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Talk]), UserModule],
+  imports: [TypeOrmModule.forFeature([Talk]), UserModule, RedisModule],
   controllers: [TalkController],
   providers: [TalkService]
 })
