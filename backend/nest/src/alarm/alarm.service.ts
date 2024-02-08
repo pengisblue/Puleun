1f809dafa0308ee8bd669c44703e52c38de3ed39
박종국 <whdrnrdl789@naver.com>
2024-02-08 10:33:18 +0900

    refactor: cron and console.log
    Change-Id: I2dd78f1e4a9455f6157394cbf12921223779470a

--- backend/nest/src/alarm/alarm.service.ts
+++ backend/nest/src/alarm/alarm.service.ts
@@ -29,8 +29,8 @@ export class AlarmService {
     // 초 분 시 일 월 요일
     // 0 - 6 일요일부터 토요일까지
     // 0 30 7 * * 1,3,6 -> 월 수 토 7시 30분 00초에 실행
-    @Cron('0 * * * * *')
-    handleCron(){
-        console.log('Cron Test!');
-    }
+    // @Cron('0 * * * * *')
+    // handleCron(){
+    //     console.log('Cron Test!');
+    // }
 }
