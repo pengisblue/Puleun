241ca7577bca5293c4c7ad26ed02fcb8602c5917
JongKook <whdrnrdl789@naver.com>
2024-02-13 23:05:45 +0900

    docs: pot-state Swagger
    Change-Id: I92988bdecbd0a69fcf782a0159c56bd299483211

--- backend/nest/src/pot-state/pot-state.controller.ts
+++ backend/nest/src/pot-state/pot-state.controller.ts
@@ -1,8 +1,7 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { PotStateService } from './pot-state.service';
 import { PotState } from './pot-state.entity';
-import { CreatePotStateDto } from './pot-state-insert.dto';
 import { StatusResultDto } from './pot-state.dto';
 
 @Controller('pot-state')
@@ -10,14 +9,9 @@ import { StatusResultDto } from './pot-state.dto';
 export class PotStateController {
     constructor(private readonly potStateService: PotStateService){};
 
-    @Get('/:pot_id/:isTemp')
+    @Get('/:pot_id/:isTemp')    
     async findById(@Param('pot_id') pot_id:number, @Param('isTemp') isTemp:boolean): Promise<PotState[]>{
         return this.potStateService.findByPotId(pot_id, isTemp)
-    }
-
-    @Post()
-    async save(@Body() createPotStateDto:CreatePotStateDto): Promise<number>{
-        return this.potStateService.save(createPotStateDto)
     }
     
     @Get(':parent_id')
@@ -29,7 +23,7 @@ export class PotStateController {
     }
 
     
-    @Post(':pot_id')
+    @Get('yesterday/:pot_id')
     @ApiOperation({summary: '화분의 전날 온습도 리스트'})
     async getDetail(@Param('pot_id') pot_id: number): Promise<any>{
         return await this.potStateService.yesterdayMoisAndTemp(pot_id);
