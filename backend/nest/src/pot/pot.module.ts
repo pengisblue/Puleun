e00fd3435ab06e4d954937b7bb2b497ef7af025d
최진우 <chjnw0204@gmail.com>
2024-02-15 12:21:10 +0900

    feat: remeasure when read potdetail
    Change-Id: I104db30989df79b0a930fd6213def42216db3e31

--- backend/nest/src/pot/pot.module.ts
+++ backend/nest/src/pot/pot.module.ts
@@ -8,9 +8,11 @@ import { PotStateModule } from 'src/pot-state/pot-state.module';
 import { CalenderModule } from 'src/calender/calender.module';
 import { DeviceModule } from 'src/device/device.module';
 import { S3Module } from 'src/s3/s3.module';
+import { SocketModule } from 'src/socket/socket.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Pot]), UserModule, CalenderModule, DeviceModule, S3Module, CalenderModule, UserModule,
+  imports: [TypeOrmModule.forFeature([Pot]), UserModule, CalenderModule, DeviceModule, 
+    S3Module, CalenderModule, UserModule, forwardRef(() =>SocketModule),
   forwardRef(() =>PotStateModule)],
   controllers: [PotController],
   providers: [PotService],
